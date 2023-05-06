e48fc5ecc63fa388434b19b09e91e877d1a9da16
Bob Nicholson <bobniklv@gmail.com>
2023-05-06 23:35:45 +0300

    Updated 2nd game

--- game_2/game.ino
+++ game_2/game.ino
@@ -1,34 +1,36 @@
-#include <Stepper.h>
+#include <CheapStepper.h>
 
 // Define the number of steps per revolution for the stepper motor
 const int stepsPerRevolution = 2048;
 
-// Initialize the Stepper library with the motor interface type and pin 
-connections
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+// Initialize the CheapStepper library with the motor interface type and pin connections
+CheapStepper myStepper(22, 24, 26, 28); // Modify the pin connections 
+according to your setup
 
 // Button pins
-const int rotateButtonPin = 2;
-const int resetButtonPin = 3;
+const int rotateButtonPin = A1;
+const int resetButtonPin = A2;
 int rotateButtonState = HIGH;
 int lastRotateButtonState = HIGH;
 int resetButtonState = HIGH;
 int lastResetButtonState = HIGH;
 
 // Angle increment
-const int angleIncrement = 45;  // Adjust this value to change the angle 
-increment
+const int angleIncrement = 720;  // Adjust this value to change the angle increment
 
 // Total angle
 int totalAngle = 0;
 
 void setup() {
   // Set the speed of the motor (in RPM)
-  myStepper.setSpeed(60);  // Adjust this value to change the motor speed
+  myStepper.setRpm(20);  // Adjust this value to change the motor speed
 
   // Initialize the button pins
   pinMode(rotateButtonPin, INPUT_PULLUP);
   pinMode(resetButtonPin, INPUT_PULLUP);
+
+  // Start serial communication
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -39,8 +41,10 @@ void loop() {
   if (rotateButtonState != lastRotateButtonState) {
     // If the rotate button is pressed (LOW state)
     if (rotateButtonState == LOW) {
+      Serial.println("Rotate button pressed");
+
       // Rotate the motor by the specified angle increment
-      myStepper.step(angleIncrement);
+      myStepper.moveDegrees(true, angleIncrement); // Rotate clockwise
       // Add the angle increment to the total angle
       totalAngle += angleIncrement;
     }
@@ -56,8 +60,10 @@ void loop() {
   if (resetButtonState != lastResetButtonState) {
     // If the reset button is pressed (LOW state)
     if (resetButtonState == LOW) {
+      Serial.println("Reset button pressed");
+
       // Rotate the motor in the reverse direction by the total angle
-      myStepper.step(-totalAngle);
+      myStepper.moveDegrees(false, totalAngle); // Rotate counter-clockwise
       // Reset the total angle to 0
       totalAngle = 0;
     }
@@ -70,3 +76,4 @@ void loop() {
   lastRotateButtonState = rotateButtonState;
   lastResetButtonState = resetButtonState;
 }
+
